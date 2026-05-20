68be8aab192958ec902f46f275c9bae2e8e658b3
Ryan Ekdahl <rekdahl@calpoly.edu>
2026-05-19 21:13:30 -0700

    updated description, namespace, and strplit file

--- R/strsplit.R
+++ R/strsplit.R
@@ -1,3 +1,14 @@
+#' Splits a String
+#'
+#' @param x A character vector with one element.
+#' @param split What element to split the string on.
+#'
+#' @returns A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
